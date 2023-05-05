aac28504b530f0045bcebb27d77818aa365403aa
HuanMinpopcorn <hmin10@jhu.edu>
2023-05-05 11:49:49 -0400

    two location added

--- turtlebot_actionlib/src/maze_client.cpp
+++ turtlebot_actionlib/src/maze_client.cpp
@@ -12,6 +12,9 @@ int main( int argc, char** argv ){
   W1.header.frame_id = "map";
   W1.pose.position.x = 1.62;
   W1.pose.position.y = -1.84;
+  W2.header.frame_id = "map";
+  W2.pose.position.x = 1;
+  W2.pose.position.y = -1.2;
   client->call(start,end,P1,P2,P3,P4,W1,W2);
   rclcpp::spin(client);
   rclcpp::shutdown();
--- turtlebot_actionlib/src/waffle_action.cpp
+++ turtlebot_actionlib/src/waffle_action.cpp
@@ -36,13 +36,12 @@ namespace turtlebot_action{
   (const std::shared_ptr<rclcpp_action::ServerGoalHandle<WaffleAction>>
    goal_handle ){
 
-    std::cout << "Waffle server processing" << std::endl;
+    /* std::cout << "Waffle server processing" << std::endl; */
 
     auto feedback = std::make_shared<WaffleAction::Feedback>();
     auto goal = goal_handle -> get_goal();
     //call NavToPose
     NavToPose_client -> call_server(goal->target);
-    feedback->progress = "feedback:call nav2 server";
     // get result
     auto result = std::make_shared<WaffleAction::Result>();
     std::cout << "Waffle is moving to target. Please Wait...\n";
@@ -50,13 +49,14 @@ namespace turtlebot_action{
     result->result = NavToPose_client -> get_result();
     if (result->result == 1){
       goal_handle -> succeed(result);
-      feedback->progress = "feedback:finish waffle nav2 path";}
+      feedback->progress = "finish waffle nav2 path";
+      goal_handle->publish_feedback(feedback);}
     else {
       goal_handle -> abort(result);
-      feedback->progress = "feedback:abort waffle nav2 path";
+      feedback->progress = "abort waffle nav2 path";
+      goal_handle->publish_feedback(feedback);
     }
 
   }
-  /* double calculate_progress(const geometry_msgs::msg) */
 }
 
