f4d30445ff2edfd3a0cdce600cd239fed95a5326
JimLiu35 <gliu35@jhu.edu>
2023-05-11 22:52:38 -0400

    can spawn two robots without frame prefix

--- final_project/launch/navigation_multibot/navigation2.launch.py
+++ final_project/launch/navigation_multibot/navigation2.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
     rviz_config_dir = os.path.join(
         get_package_share_directory('final_project'),
         'rviz',
-        'simulation_nav2.rviz')
+        'simulation_multibots.rviz')
 
     return LaunchDescription([
         DeclareLaunchArgument(
